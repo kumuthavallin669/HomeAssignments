ed551540913d0d9341acddbd49c86f1ee76d73d4
kumuthavallin669 <kumuthavallin43@gmail.com>
2025-10-15 00:20:56 +0530

    Add files via upload
    Week 1 Day 2 Assignments

---
+++ day2/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		int b = 1;
+		int c=0;
+		for(int i=1;i<=8;i++)
+		{
+			System.out.println(a);
+			c = a +b;
+			a=b;
+			b=c;
+		
+		
+	}
+
+}
+}
